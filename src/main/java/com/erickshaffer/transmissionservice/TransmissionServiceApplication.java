84d3032802122e98eff25af6dad2033222548a09
Erick Shaffer <erickshaffer@Ericks-MacBook-Pro.local>
2019-06-08 13:00:57 -0700

    inital spring-boot commit

---
+++ src/main/java/com/erickshaffer/transmissionservice/TransmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erickshaffer.transmissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransmissionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erickshaffer/transmissionservice/TransmissionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.erickshaffer.transmissionservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransmissionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
